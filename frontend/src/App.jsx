676cc8ffc5c19bdfed26b5d329a08b8c2446c97d
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-25 10:24:01 +0300

    DRD-31: Install mautic-tracking dependency, connect mautic instance and react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import About from "./pages/About";
 import Layout from "./pages/Layout";
 import Contact from "./pages/Contact";
@@ -6,21 +6,29 @@ import Services from "./pages/Services";
 import Jobs from "./pages/Jobs";
 import Blog from "./pages/Blog";
 import FrontPage from "./pages/FrontPage";
+import mautic from "./services/mautic";
+import { useEffect } from "react";
 
 const App = () => {
+  const location = useLocation();
+
+  // Track page views whenever the route changes
+  useEffect(() => {
+    // Track page views when the location changes
+    mautic.pageView({ path: location.pathname });
+  }, [location]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<FrontPage />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/blog" element={<Blog />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<FrontPage />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/blog" element={<Blog />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ frontend/src/services/mautic.jsx
@@ -0,0 +1,9 @@
+import mautic from "mautic-tracking";
+
+// Replace with your Mautic instance's localhost address. Omit trailing forward slash.
+const mauticUrl = "http://localhost:62208";
+
+// Initialize the Mautic Tracking
+mautic.initialize(`${mauticUrl}/mtc.js`);
+
+export default mautic;
