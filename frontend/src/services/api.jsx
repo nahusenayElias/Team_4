b32040a0e881fe8b59c403d4db7bd0fe4e0706b5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-28 13:15:54 +0200

    Export configs, dump db

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const localhostAddress = "http://localhost:56050"; // Update this if your localhost address changes. No trailing forward slash.
+const localhostAddress = "http://localhost:53144"; // Update this if your localhost address changes. No trailing forward slash.
 const API_URL = `${localhostAddress}/jsonapi`;
 
 export const fetchContent = async (contentType) => {
