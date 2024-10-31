b3f083f78893621ecd73f4a18a85f224921d42cb
Elias Hagos <eliasars@yahoo.com>
2024-10-31 12:32:36 +0200

    Additional commits to DRD-17

--- frontend/src/services/mautic.jsx
+++ frontend/src/services/mautic.jsx
@@ -1,7 +1,7 @@
 import mautic from "mautic-tracking";
 
 // Replace with your Mautic instance's localhost address. Omit trailing forward slash.
-const mauticUrl = "http://localhost:55798";
+const mauticUrl = "http://localhost:55793";
 
 // Initialize the Mautic Tracking
 mautic.initialize(`${mauticUrl}/mtc.js`);
