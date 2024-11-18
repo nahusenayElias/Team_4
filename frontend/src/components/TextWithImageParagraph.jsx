df6a33e475a32a8e37773a15b2c6513e47100c0f
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-11-18 11:21:00 +0200

    DRD-81: Check 'field_image_first' and render paragraph layout accordingly

--- frontend/src/components/TextWithImageParagraph.jsx
+++ frontend/src/components/TextWithImageParagraph.jsx
@@ -4,8 +4,20 @@ import { findImageUrl } from "../services/utils";
 const TextWithImageParagraph = ({ paragraph, included }) => {
   const imageUrl = findImageUrl(paragraph.relationships?.field_image, included);
 
+  // Check if toggle "field_image_first" is set to true or false by user in drupal paragraph:
+  const isImageFirst = paragraph.attributes?.field_image_first || false;
+
   return (
     <div className="flex flex-col md:flex-row items-center gap-6">
+      {isImageFirst && imageUrl && (
+        <div className="md:w-1/2">
+          <img
+            src={imageUrl}
+            alt={paragraph.attributes?.field_heading || "Project image"}
+            className="max-w-full h-auto rounded-lg shadow-md"
+          />
+        </div>
+      )}
       <div className="md:w-1/2">
         <h3 className="font-semibold">
           {paragraph.attributes?.field_heading || ""}
@@ -13,19 +25,19 @@ const TextWithImageParagraph = ({ paragraph, included }) => {
         <div
           className="prose"
           dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(paragraph.attributes?.field_text?.value),
+            __html: DOMPurify.sanitize(paragraph.attributes?.field_long_text),
           }}
         />
       </div>
-      <div className="md:w-1/2">
-        {imageUrl && (
+      {!isImageFirst && imageUrl && (
+        <div className="md:w-1/2">
           <img
             src={imageUrl}
             alt={paragraph.attributes?.field_heading || "Project image"}
             className="max-w-full h-auto rounded-lg shadow-md"
           />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
