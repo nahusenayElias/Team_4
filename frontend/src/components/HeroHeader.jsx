89fd0d28ad9656ddcc8cdc5849a78ccaf09c7ee4
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-12-16 10:36:29 +0200

    DRD-119: Increase top margin, add font size breakpoints

--- frontend/src/components/HeroHeader.jsx
+++ frontend/src/components/HeroHeader.jsx
@@ -1,14 +1,14 @@
 const HeroHeader = ({ imageUrl, content }) => {
   return (
     <div
-      className="h-96 bg-cover bg-center flex justify-center items-center"
+      className="h-96 mt-16 bg-cover bg-center flex justify-center items-center"
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${imageUrl})`,
       }}
     >
       <div className="flex flex-col justify-center items-center p-4">
         {content?.attributes?.title && (
-          <h1 className="text-5xl font-sans font-semibold text-white text-center max-w-3xl leading-tight">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-sans font-semibold text-white text-center max-w-3xl leading-tight">
             {content.attributes.title}
           </h1>
         )}
