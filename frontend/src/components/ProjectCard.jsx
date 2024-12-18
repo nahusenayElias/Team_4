4f205de8183b59375b30f271049ab4fdb311f8a8
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-12-18 11:27:38 +0200

    DRD-129: Remove line-clamp, only display first two sentences of description

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -5,28 +5,26 @@ const ProjectCard = ({ project }) => {
   const companyName = customer?.toLowerCase().replace(/\s+/g, "-");
 
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full h-full">
-      {heroImageUrl ? (
-        <img
-          src={heroImageUrl}
-          alt={heroImageAltText}
-          className="w-full h-48 object-cover"
-        />
-      ) : (
-        <div className="w-full h-48 bg-gray-300 flex items-center justify-center text-white">
-          No image available
-        </div>
-      )}
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full h-full flex flex-col">
+      <div className="h-64">
+        {heroImageUrl ? (
+          <img
+            src={heroImageUrl}
+            alt={heroImageAltText}
+            className="w-full h-full object-cover"
+          />
+        ) : null}
+      </div>
 
-      <div className="p-6">
+      <div className="p-6 flex flex-col flex-grow">
         {/* <h2 className="text-xl font-semibold text-gray-800">{title}</h2> */}
         <h3 className="text-xl text-gray-600 mb-2">{customer}</h3>
-        <p className="text-gray-500 text-sm md:text-base line-clamp-3">
-          {description}
+        <p className="text-gray-500 text-sm md:text-base flex-grow mb-3">
+          {description.split(". ").slice(0, 2).join(". ")}
         </p>
         <Link
           to={`/projects/${companyName}`} // Link to the detailed page using company name
-          className="text-orange-500 hover:text-orange-700 mt-4 block text-sm font-semibold"
+          className="text-orange-500 hover:text-orange-700 block text-sm font-semibold mt-auto"
         >
           Read More{" "}
         </Link>
